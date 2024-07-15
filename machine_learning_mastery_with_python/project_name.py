309b2298ee553e9562ea332dc7e455b342dd846f
WyattGwyon <claytonlouden@gmail.com>
2024-07-15 22:56:07 +0200

    Lessons finished, first two end-to-end projects complete

---
+++ machine_learning_mastery_with_python/project_name.py
@@ -0,0 +1,29 @@
+# Python Project Template
+
+# 1. Prepare Problem
+# a) Load Libraries
+# b) Load Dataset
+
+# 2. Summarize Data
+# a) Descriptive Statistics
+# b) Data Visualization
+
+# 3. Prepare Date
+# a) Data Cleaning
+# b) Feature Selection
+# c) Data Transforms (splits/folds)
+
+# 4. Evaluate Algorithms
+# a) Split-out Validation Dataset
+# b) Test options and Evaluate Metrics
+# c) Spot Check Algorithms
+# d) Compare Algorithms
+
+# 5. Improve Accuracy
+# a) Algorithm Tuning
+# b) Ensembles
+
+# 6. Finalize Model
+# a) Predictions on vvalidation dataset
+# b) Create standalone Model on entire training dataset
+# c) Save Model for later use
